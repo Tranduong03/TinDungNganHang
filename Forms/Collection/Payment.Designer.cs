f9709e8481d99659363d019ae2ab34013b9a81e8
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-04-24 15:01:28 +0700

    [fix]: sửa form Payment

--- Forms/Collection/Payment.Designer.cs
+++ Forms/Collection/Payment.Designer.cs
@@ -45,7 +45,7 @@
             lblTitle.AutoSize = true;
             lblTitle.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
             lblTitle.ForeColor = Color.MediumBlue;
-            lblTitle.Location = new Point(170, 20);
+            lblTitle.Location = new Point(160, 20);
             lblTitle.Name = "lblTitle";
             lblTitle.Size = new Size(361, 54);
             lblTitle.TabIndex = 0;
@@ -53,64 +53,69 @@
             // 
             // lblLoanId
             // 
+            lblLoanId.AutoSize = true;
             lblLoanId.Font = new Font("Segoe UI", 12F);
-            lblLoanId.Location = new Point(26, 283);
+            lblLoanId.Location = new Point(50, 100);
             lblLoanId.Name = "lblLoanId";
-            lblLoanId.Size = new Size(100, 23);
+            lblLoanId.Size = new Size(100, 32);
             lblLoanId.TabIndex = 1;
             lblLoanId.Text = "Loan ID:";
             // 
             // txtLoanId
             // 
+            txtLoanId.BorderStyle = BorderStyle.None;
             txtLoanId.Font = new Font("Segoe UI", 12F);
-            txtLoanId.Location = new Point(201, 391);
+            txtLoanId.Location = new Point(210, 95);
             txtLoanId.Name = "txtLoanId";
             txtLoanId.ReadOnly = true;
-            txtLoanId.Size = new Size(300, 39);
+            txtLoanId.Size = new Size(320, 32);
             txtLoanId.TabIndex = 2;
             // 
             // lblCustomerName
             // 
+            lblCustomerName.AutoSize = true;
             lblCustomerName.Font = new Font("Segoe UI", 12F);
-            lblCustomerName.Location = new Point(12, 170);
+            lblCustomerName.Location = new Point(50, 160);
             lblCustomerName.Name = "lblCustomerName";
-            lblCustomerName.Size = new Size(100, 23);
+            lblCustomerName.Size = new Size(122, 32);
             lblCustomerName.TabIndex = 3;
             lblCustomerName.Text = "Customer:";
             // 
             // txtCustomerName
             // 
             txtCustomerName.Font = new Font("Segoe UI", 12F);
-            txtCustomerName.Location = new Point(288, 301);
+            txtCustomerName.Location = new Point(210, 155);
             txtCustomerName.Name = "txtCustomerName";
             txtCustomerName.ReadOnly = true;
-            txtCustomerName.Size = new Size(300, 39);
+            txtCustomerName.Size = new Size(320, 39);
             txtCustomerName.TabIndex = 4;
             // 
             // lblAmountOwed
             // 
+            lblAmountOwed.AutoSize = true;
             lblAmountOwed.Font = new Font("Segoe UI", 12F);
-            lblAmountOwed.Location = new Point(220, 137);
+            lblAmountOwed.Location = new Point(50, 220);
             lblAmountOwed.Name = "lblAmountOwed";
-            lblAmountOwed.Size = new Size(100, 23);
+            lblAmountOwed.Size = new Size(174, 32);
             lblAmountOwed.TabIndex = 5;
             lblAmountOwed.Text = "Amount Owed:";
             // 
             // txtAmountOwed
             // 
             txtAmountOwed.Font = new Font("Segoe UI", 12F);
-            txtAmountOwed.Location = new Point(240, 219);
+            txtAmountOwed.Location = new Point(210, 215);
             txtAmountOwed.Name = "txtAmountOwed";
             txtAmountOwed.ReadOnly = true;
-            txtAmountOwed.Size = new Size(300, 39);
+            txtAmountOwed.Size = new Size(320, 39);
             txtAmountOwed.TabIndex = 6;
             // 
             // lblPaymentAmount
             // 
+            lblPaymentAmount.AutoSize = true;
             lblPaymentAmount.Font = new Font("Segoe UI", 12F);
-            lblPaymentAmount.Location = new Point(47, 37);
+            lblPaymentAmount.Location = new Point(50, 280);
             lblPaymentAmount.Name = "lblPaymentAmount";
-            lblPaymentAmount.Size = new Size(100, 23);
+            lblPaymentAmount.Size = new Size(204, 32);
             lblPaymentAmount.TabIndex = 7;
             lblPaymentAmount.Text = "Payment Amount:";
             // 
@@ -118,9 +123,9 @@
             // 
             txtPaymentAmount.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             txtPaymentAmount.ForeColor = Color.DarkGreen;
-            txtPaymentAmount.Location = new Point(220, 80);
+            txtPaymentAmount.Location = new Point(210, 275);
             txtPaymentAmount.Name = "txtPaymentAmount";
-            txtPaymentAmount.Size = new Size(300, 39);
+            txtPaymentAmount.Size = new Size(320, 39);
             txtPaymentAmount.TabIndex = 8;
             // 
             // btnPay
@@ -128,9 +133,9 @@
             btnPay.BackColor = Color.LawnGreen;
             btnPay.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
             btnPay.ForeColor = Color.Black;
-            btnPay.Location = new Point(431, 164);
+            btnPay.Location = new Point(330, 350);
             btnPay.Name = "btnPay";
-            btnPay.Size = new Size(120, 45);
+            btnPay.Size = new Size(100, 45);
             btnPay.TabIndex = 9;
             btnPay.Text = "✔ Pay";
             btnPay.UseVisualStyleBackColor = false;
@@ -139,9 +144,9 @@
             // 
             btnBack.BackColor = Color.LightGray;
             btnBack.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
-            btnBack.Location = new Point(300, 80);
+            btnBack.Location = new Point(210, 350);
             btnBack.Name = "btnBack";
-            btnBack.Size = new Size(120, 45);
+            btnBack.Size = new Size(100, 45);
             btnBack.TabIndex = 10;
             btnBack.Text = "← Back";
             btnBack.UseVisualStyleBackColor = false;
