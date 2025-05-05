1a7d32b607f18d9dab5393bb9b2e5ea54acfc608
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-05-06 02:22:02 +0700

    [fix]: fix UI_2

--- Forms/Home/Home.Designer.cs
+++ Forms/Home/Home.Designer.cs
@@ -30,13 +30,28 @@
             btnThuNo = new Button();
             btnDuyetVay = new Button();
             panelMain = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
             panelMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
             SuspendLayout();
             // 
             // panelMenu
             // 
             panelMenu.BackColor = Color.LightSkyBlue;
+            panelMenu.Controls.Add(pictureBox6);
+            panelMenu.Controls.Add(pictureBox5);
+            panelMenu.Controls.Add(pictureBox4);
+            panelMenu.Controls.Add(pictureBox3);
+            panelMenu.Controls.Add(pictureBox2);
             panelMenu.Controls.Add(lblName);
             panelMenu.Controls.Add(pictureBox1);
             panelMenu.Controls.Add(btnExit);
@@ -74,53 +89,63 @@
             // 
             // btnExit
             // 
-            btnExit.Font = new Font("Segoe UI", 14F);
-            btnExit.Location = new Point(0, 1000);
+            btnExit.BackColor = Color.White;
+            btnExit.Font = new Font("Bookman Old Style", 15F);
+            btnExit.Location = new Point(0, 920);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(400, 60);
+            btnExit.Size = new Size(400, 70);
             btnExit.TabIndex = 4;
-            btnExit.Text = "Exit";
+            btnExit.Text = "    Đăng Xuất";
+            btnExit.UseVisualStyleBackColor = false;
             btnExit.Click += btnExit_Click;
             // 
             // btn_HomePanel
             // 
-            btn_HomePanel.Font = new Font("Segoe UI", 14F);
-            btn_HomePanel.Location = new Point(0, 520);
+            btn_HomePanel.BackColor = Color.White;
+            btn_HomePanel.Font = new Font("Bookman Old Style", 15F);
+            btn_HomePanel.Location = new Point(0, 400);
             btn_HomePanel.Name = "btn_HomePanel";
-            btn_HomePanel.Size = new Size(400, 60);
+            btn_HomePanel.Size = new Size(400, 70);
             btn_HomePanel.TabIndex = 3;
-            btn_HomePanel.Text = "Home";
+            btn_HomePanel.Text = "Trang Chủ";
             btn_HomePanel.TextImageRelation = TextImageRelation.TextBeforeImage;
+            btn_HomePanel.UseVisualStyleBackColor = false;
             btn_HomePanel.Click += btn_HomePanel_Click;
             // 
             // btnKhachHang
             // 
-            btnKhachHang.Font = new Font("Segoe UI", 14F);
-            btnKhachHang.Location = new Point(0, 880);
+            btnKhachHang.BackColor = Color.White;
+            btnKhachHang.Font = new Font("Bookman Old Style", 15F);
+            btnKhachHang.Location = new Point(0, 530);
             btnKhachHang.Name = "btnKhachHang";
-            btnKhachHang.Size = new Size(400, 60);
+            btnKhachHang.Size = new Size(400, 70);
             btnKhachHang.TabIndex = 0;
-            btnKhachHang.Text = "Profile Customer";
+            btnKhachHang.Text = "    Quản Lí Đơn Vay";
+            btnKhachHang.UseVisualStyleBackColor = false;
             btnKhachHang.Click += btnKhachHang_Click;
             // 
             // btnThuNo
             // 
-            btnThuNo.Font = new Font("Segoe UI", 14F);
-            btnThuNo.Location = new Point(0, 640);
+            btnThuNo.BackColor = Color.White;
+            btnThuNo.Font = new Font("Bookman Old Style", 15F);
+            btnThuNo.Location = new Point(0, 660);
             btnThuNo.Name = "btnThuNo";
-            btnThuNo.Size = new Size(400, 60);
+            btnThuNo.Size = new Size(400, 70);
             btnThuNo.TabIndex = 1;
-            btnThuNo.Text = "Thu nợ";
+            btnThuNo.Text = "Quản Lí Thu Nợ";
+            btnThuNo.UseVisualStyleBackColor = false;
             btnThuNo.Click += btnThuNo_Click;
             // 
             // btnDuyetVay
             // 
-            btnDuyetVay.Font = new Font("Segoe UI", 14F);
-            btnDuyetVay.Location = new Point(0, 760);
+            btnDuyetVay.BackColor = Color.White;
+            btnDuyetVay.Font = new Font("Bookman Old Style", 15F);
+            btnDuyetVay.Location = new Point(0, 790);
             btnDuyetVay.Name = "btnDuyetVay";
-            btnDuyetVay.Size = new Size(400, 60);
+            btnDuyetVay.Size = new Size(400, 70);
             btnDuyetVay.TabIndex = 2;
-            btnDuyetVay.Text = "Duyệt vay";
+            btnDuyetVay.Text = "Quản Lí Sổ Nợ";
+            btnDuyetVay.UseVisualStyleBackColor = false;
             btnDuyetVay.Click += btnDuyetVay_Click;
             // 
             // panelMain
@@ -131,6 +156,62 @@
             panelMain.Name = "panelMain";
             panelMain.Size = new Size(1600, 1250);
             panelMain.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = SystemColors.Control;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(10, 925);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(60, 60);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = SystemColors.Control;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(10, 795);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(60, 60);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = SystemColors.Control;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(10, 665);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(60, 60);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = SystemColors.Control;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(10, 535);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(60, 60);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 10;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = SystemColors.Control;
+            pictureBox6.BackgroundImage = (Image)resources.GetObject("pictureBox6.BackgroundImage");
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(10, 405);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(60, 60);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 11;
+            pictureBox6.TabStop = false;
             // 
             // Home
             // 
@@ -148,6 +229,11 @@
             panelMenu.ResumeLayout(false);
             panelMenu.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
             ResumeLayout(false);
         }
 
@@ -155,5 +241,10 @@
         private Button btnExit;
         private PictureBox pictureBox1;
         private Label lblName;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
     }
 }
